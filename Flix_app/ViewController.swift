1d10b8d3882a2699d39446a7e40c6473b0e5dd57
Eliza Vardanyan <evarda17@gmail.com>
2020-10-22 17:53:34 -0400

    Initial Commit

---
+++ Flix_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix_app
+//
+//  Created by Eliza Vardanyan on 10/22/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
